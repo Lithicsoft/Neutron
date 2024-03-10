3882052fbd760574d70181f887dc4b12064813b3
Bùi Nguyễn Tấn Sang <endermatday@gmail.com>
2024-03-10 21:14:12 +0700

    SF Patch PART B2
    Bring language filter back

--- app/search.py
+++ app/search.py
@@ -1,4 +1,5 @@
 from app import app
+from langdetect import detect
 from flask import render_template, request
 
 from search.get import Search_Data
@@ -14,20 +15,42 @@ def index():
 def search():
     keyword = request.args.get('q', '')
     type = request.args.get('type', '')
+    language_hl = request.args.get('hl', '')
+    time_t = request.args.get('t', '')
+
+    search_result = []
+
     if type == '':
         type = 'Text'
-    
-    search_result = Search_Data(type, keyword)
-    print(search_result)
+    if language_hl != '':
+        search_result = Search_Data(type, keyword)
+        filtered_data = []
+        for item in search_result:
+            try:
+                language1 = detect(item[1])
+                language2 = detect(item[5])
+            except:
+                continue 
+            
+            if language1 == language_hl or language2 == language_hl:
+                filtered_data.append(item)
+        search_result = filtered_data
+    else:
+        search_result = Search_Data(type, keyword)
+    if time_t == '':
+        pass
+
     if search_result == []:
         return render_template(
             '/search/index.html',
             query=keyword,
+            languages = ['all', 'af', 'ar', 'bg', 'bn', 'ca', 'cs', 'cy', 'da', 'de', 'el', 'en', 'es', 'et', 'fa', 'fi', 'fr', 'gu', 'he', 'hi', 'hr', 'hu', 'id', 'it', 'ja', 'kn', 'ko', 'lt', 'lv', 'mk', 'ml', 'mr', 'ne', 'nl', 'no', 'pa', 'pl', 'pt', 'ro', 'ru', 'sk', 'sl', 'so', 'sq', 'sv', 'sw', 'ta', 'te', 'th', 'tl', 'tr', 'uk', 'ur', 'vi', 'zh-cn', 'zh-tw'],
             note='No results found.'
         )
     else:
         return render_template(
             '/search/index.html',
             query=keyword,
+            languages = ['all', 'af', 'ar', 'bg', 'bn', 'ca', 'cs', 'cy', 'da', 'de', 'el', 'en', 'es', 'et', 'fa', 'fi', 'fr', 'gu', 'he', 'hi', 'hr', 'hu', 'id', 'it', 'ja', 'kn', 'ko', 'lt', 'lv', 'mk', 'ml', 'mr', 'ne', 'nl', 'no', 'pa', 'pl', 'pt', 'ro', 'ru', 'sk', 'sl', 'so', 'sq', 'sv', 'sw', 'ta', 'te', 'th', 'tl', 'tr', 'uk', 'ur', 'vi', 'zh-cn', 'zh-tw'],
             results=search_result
         )
--- app/templates/search/script.js
+++ app/templates/search/script.js
@@ -14,6 +14,24 @@ tags.forEach(function(tag) {
   });
 });
 
+var languageSelect = document.querySelector('#languageSelect');
+
+var params = new URLSearchParams(window.location.search);
+var hl = params.get('hl');
+if (hl) {
+  languageSelect.value = hl;
+}
+
+languageSelect.addEventListener('change', function() {
+  var lang = this.options[this.selectedIndex].getAttribute('result-language');
+  
+  params.set('hl', lang);
+  
+  window.history.replaceState({}, '', '?' + params.toString());
+
+  window.location.reload();
+});
+
 window.onload = function() {
   var resultDiv = document.querySelector('.result');
   if (resultDiv.innerHTML.trim() === '') {
