5b6708ca54213f145bbcd2660ad5150af5cf2c66
Bùi Nguyễn Tấn Sang <endermatday@gmail.com>
2024-05-11 19:22:42 +0700

    Change template folder to frontend folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_babel import Babel
 from app import language
 
-app = Flask(__name__)
+frontend_dir = os.path.join("./", 'frontend')
+app = Flask(__name__, template_folder=frontend_dir)
 babel = Babel(app, locale_selector=language.get_locale)
 
 from app import databases, ext, search, contribute, account
